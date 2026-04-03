408642cc764f5985c6179f16a4971c574184a94a
clearmyrecord <matttunstall3@gmail.com>
2026-04-03 11:12:35 -0700

    Create form-data.js

---
+++ js/form-data.js
@@ -0,0 +1,17 @@
+function getApplicantData() {
+  return {
+    fullName: "Matt Tunstall",
+    mailingAddress: "123 Main St, Henderson, NV 89012",
+    county: "Wood County",
+    state: "Ohio",
+    caseNumber: "2006CR0387",
+    offenseType: "Drug possession",
+    offenseLevel: "3rd Degree Felony",
+    finalDischargeDate: "12/10/2010",
+    courtName: "Wood County Court of Common Pleas",
+    violentOffense: "No",
+    openCases: "No",
+    finesPaid: "Yes",
+    convictionsInvolved: "1"
+  };
+}
